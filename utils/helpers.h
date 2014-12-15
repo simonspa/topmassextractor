e7177560c97b759ec66ec747416cac439f5eb02c
Simon Spannagel <simon.spannagel@desy.de>
2014-12-15 19:06:47 +0100

    Add new flag to utils to be printed.

--- utils/helpers.h
+++ utils/helpers.h
@@ -25,6 +25,7 @@ namespace massextractor {
     if((flags&FLAG_DONT_SUBTRACT_BACKGROUND) != 0) { os << "FLAG_DONT_SUBTRACT_BACKGROUND, "; flags -= FLAG_DONT_SUBTRACT_BACKGROUND; }
     if((flags&FLAG_NO_THEORYPREDICTION_ERRORS) != 0) { os << "FLAG_NO_THEORYPREDICTION_ERRORS, "; flags -= FLAG_NO_THEORYPREDICTION_ERRORS; }
     if((flags&FLAG_IGNORE_MC_STATERR) != 0) { os << "FLAG_IGNORE_MC_STATERR, "; flags -= FLAG_IGNORE_MC_STATERR; }
+    if((flags&FLAG_INFINITE_DATA_STATISTICS) != 0) { os << "FLAG_INFINITE_DATA_STATISTICS, "; flags -= FLAG_INFINITE_DATA_STATISTICS; }
 
     if(flags != 0) os << "Unknown flag: " << flags;
     return os.str();
