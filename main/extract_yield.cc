1fcff7570221a2a9d5c93dc6719e661ef478a42c
Simon Spannagel <simon.spannagel@desy.de>
2015-04-17 16:12:12 +0200

    Rework yield extraction executable to get masses in pairs. much simpler.

--- main/extract_yield.cc
+++ main/extract_yield.cc
@@ -30,43 +30,35 @@ void massextractor::extract_yield(TString inputpath, TString outputpath, std::ve
   std::vector<TString> syst_bg;
   std::vector<TString> systematics;
 
-  syst_on_nominal.push_back("MATCH_UP");
-  syst_on_nominal.push_back("MATCH_DOWN");
-  syst_on_nominal.push_back("SCALE_UP");
-  syst_on_nominal.push_back("SCALE_DOWN");
-  syst_on_nominal.push_back("HAD_UP");
-  syst_on_nominal.push_back("HAD_DOWN");
-  syst_on_nominal.push_back("CR_UP");
-  syst_on_nominal.push_back("CR_DOWN");
-  syst_on_nominal.push_back("UE_UP");
-  syst_on_nominal.push_back("UE_DOWN");
+  syst_on_nominal.push_back("MATCH");
+  syst_on_nominal.push_back("SCALE");
+  syst_on_nominal.push_back("HAD");
+  syst_on_nominal.push_back("CR");
+  syst_on_nominal.push_back("UE");
 
-  syst_bg.push_back("BG_UP");
-  syst_bg.push_back("BG_DOWN");
+  systematics.push_back("JES");
 
-  systematics.push_back("JES_UP"); systematics.push_back("JES_DOWN");
+  /*systematics.push_back("JES_MPF");
+  systematics.push_back("JES_INTERCAL");
+  systematics.push_back("JES_UNCORR");
+  systematics.push_back("JES_BJES");
 
-  /*systematics.push_back("JES_MPF_UP"); systematics.push_back("JES_MPF_DOWN");
-  systematics.push_back("JES_INTERCAL_UP"); systematics.push_back("JES_INTERCAL_DOWN");
-  systematics.push_back("JES_UNCORR_UP"); systematics.push_back("JES_UNCORR_DOWN");
-  systematics.push_back("JES_BJES_UP"); systematics.push_back("JES_BJES_DOWN");
+  systematics.push_back("JES_FLAVOR_GLUON");
+  systematics.push_back("JES_FLAVOR_QUARK");
+  systematics.push_back("JES_FLAVOR_CHARM");
+  systematics.push_back("JES_FLAVOR_BOTTOM");*/
 
-  systematics.push_back("JES_FLAVOR_GLUON_UP"); systematics.push_back("JES_FLAVOR_GLUON_DOWN");
-  systematics.push_back("JES_FLAVOR_QUARK_UP"); systematics.push_back("JES_FLAVOR_QUARK_DOWN");
-  systematics.push_back("JES_FLAVOR_CHARM_UP"); systematics.push_back("JES_FLAVOR_CHARM_DOWN");
-  systematics.push_back("JES_FLAVOR_BOTTOM_UP"); systematics.push_back("JES_FLAVOR_BOTTOM_DOWN");*/
-
-  systematics.push_back("JER_UP"); systematics.push_back("JER_DOWN");
-  systematics.push_back("PU_UP"); systematics.push_back("PU_DOWN");
-  systematics.push_back("TRIG_UP"); systematics.push_back("TRIG_DOWN");
-  systematics.push_back("KIN_UP"); systematics.push_back("KIN_DOWN");
-  systematics.push_back("LEPT_UP"); systematics.push_back("LEPT_DOWN");
-  systematics.push_back("BTAG_UP"); systematics.push_back("BTAG_DOWN");
-  systematics.push_back("BTAG_LJET_UP"); systematics.push_back("BTAG_LJET_DOWN");
-  systematics.push_back("BTAG_PT_UP"); systematics.push_back("BTAG_PT_DOWN");
-  systematics.push_back("BTAG_ETA_UP"); systematics.push_back("BTAG_ETA_DOWN");
-  systematics.push_back("BTAG_LJET_PT_UP"); systematics.push_back("BTAG_LJET_PT_DOWN");
-  systematics.push_back("BTAG_LJET_ETA_UP"); systematics.push_back("BTAG_LJET_ETA_DOWN");
+  systematics.push_back("JER");
+  systematics.push_back("PU");
+  systematics.push_back("TRIG");
+  systematics.push_back("KIN");
+  systematics.push_back("LEPT");
+  systematics.push_back("BTAG");
+  systematics.push_back("BTAG_LJET");
+  systematics.push_back("BTAG_PT");
+  systematics.push_back("BTAG_ETA");
+  systematics.push_back("BTAG_LJET_PT");
+  systematics.push_back("BTAG_LJET_ETA");
 
   for(std::vector<TString>::iterator ch = channels.begin(); ch != channels.end(); ++ch) {
 
@@ -91,6 +83,7 @@ void massextractor::extract_yield(TString inputpath, TString outputpath, std::ve
     Double_t total_theo_pos = 0;
     Double_t total_theo_neg = 0;
     Double_t var_stat_pos, var_stat_neg;
+    Double_t var_stat_pos2, var_stat_neg2;
     if(syst) {
       // We are processing systematic variations. Maybe user requested histograms for all of them?
       if(fulltake) { flags |= FLAG_STORE_HISTOGRAMS; }
@@ -99,110 +92,133 @@ void massextractor::extract_yield(TString inputpath, TString outputpath, std::ve
       for(std::vector<TString>::iterator syst = syst_on_nominal.begin(); syst != syst_on_nominal.end(); ++syst) {
 	LOG(logDEBUG) << "Getting " << (*syst) << " variation...";
 
-	extractorYieldOtherSamples * matchscale_samples = new extractorYieldOtherSamples(*ch,"Nominal", inputpath, outputpath, flags,(*syst));
-	if(closure) matchscale_samples->setClosureSample(closure_sample);
+	extractorYieldOtherSamples * variation_up = new extractorYieldOtherSamples(*ch,"Nominal", inputpath, outputpath, flags,(*syst)+"_UP");
+	extractorYieldOtherSamples * variation_down = new extractorYieldOtherSamples(*ch,"Nominal", inputpath, outputpath, flags,(*syst)+"_DOWN");
+	if(closure) {
+	  variation_up->setClosureSample(closure_sample);
+	  variation_down->setClosureSample(closure_sample);
+	}
 
-	Double_t topmass_variation = matchscale_samples->getTopMass();
-	LOG(logINFO) << *syst << " - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation;
-	Double_t delta = (Double_t)topmass-topmass_variation;
-	matchscale_samples->getStatError(var_stat_pos,var_stat_neg);
-	LOG(logRESULT) << *syst << ": delta = " << delta << " GeV +" << var_stat_pos << "-" << var_stat_neg;
-	
+	Double_t topmass_variation_up = variation_up->getTopMass();
+	Double_t topmass_variation_down = variation_down->getTopMass();
+
+	variation_up->getStatError(var_stat_pos,var_stat_neg);
+	variation_down->getStatError(var_stat_pos2,var_stat_neg2);
+
+	LOG(logINFO) << *syst << "_UP - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation_up << " +" << var_stat_pos << " -" << var_stat_neg;
+	LOG(logINFO) << *syst << "_DOWN - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation_down << " +" << var_stat_pos2 << " -" << var_stat_neg2;
+
+	Double_t delta_up = (Double_t)topmass-topmass_variation_up;
+	Double_t delta_down = (Double_t)topmass-topmass_variation_down;
+
+	LOG(logRESULT) << *syst << "_UP: delta = " << delta_up << " GeV +" << systStatErr(total_stat_pos,var_stat_pos) << "-" << systStatErr(total_stat_neg,var_stat_neg);
+	LOG(logRESULT) << *syst << "_DOWN: delta = " << delta_down << " GeV +" << systStatErr(total_stat_pos,var_stat_pos2) << "-" << systStatErr(total_stat_neg,var_stat_neg2);
+
 	if(syst->Contains("MATCH") || syst->Contains("SCALE")) {
-	  if(delta > 0) total_theo_pos += delta*delta;
-	  else total_theo_neg += delta*delta;
+	  getSystematicUpDownError(delta_up,delta_down,total_theo_pos,total_theo_neg);
+	  LOG(logDEBUG) << "Added to theory uncertainties.";
 	}
 	else {
-	  if(delta > 0) total_syst_pos += delta*delta;
-	  else total_syst_neg += delta*delta;
+	  getSystematicUpDownError(delta_up,delta_down,total_syst_pos,total_syst_neg);
+	  LOG(logDEBUG) << "Added to experimental uncertainties.";
 	}
 
-	if(syst->Contains("UP")) { SystOutputFile << systab->writeSystematicsTableUp(*syst, delta, var_stat_pos, var_stat_neg);	}
-	else { SystOutputFile << systab->writeSystematicsTableDown(delta, var_stat_pos, var_stat_neg); }
-	delete matchscale_samples;
+	//if(syst->Contains("UP")) { SystOutputFile << systab->writeSystematicsTableUp(*syst, delta, var_stat_pos, var_stat_neg);	}
+	//else { SystOutputFile << systab->writeSystematicsTableDown(delta, var_stat_pos, var_stat_neg); }
+	delete variation_up;
+	delete variation_down;
       }
 
       // Getting DrellYan/Background variations:
-      for(std::vector<TString>::iterator syst = syst_bg.begin(); syst != syst_bg.end(); ++syst) {
-	LOG(logDEBUG) << "Getting " << (*syst) << " variation...";
-	extractorYieldBackground * bg_samples = new extractorYieldBackground(*ch,"Nominal",inputpath, outputpath, flags,(*syst));
-	if(closure) bg_samples->setClosureSample(closure_sample);
-
-	Double_t topmass_variation = bg_samples->getTopMass();
-	LOG(logINFO) << *syst << " - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation;
-	Double_t delta = (Double_t)topmass-topmass_variation;
-	bg_samples->getStatError(var_stat_pos,var_stat_neg);
-	LOG(logRESULT) << *syst << ": delta = " << delta << " GeV +" << var_stat_pos << "-" << var_stat_neg;
-	if(delta > 0) total_syst_pos += delta*delta;
-	else total_syst_neg += delta*delta;
-      
-	if(syst->Contains("UP")) SystOutputFile << systab->writeSystematicsTableUp(*syst, delta, var_stat_pos, var_stat_neg);
-	else SystOutputFile << systab->writeSystematicsTableDown(delta, var_stat_pos, var_stat_neg);
+      LOG(logDEBUG) << "Getting BG variation...";
+      extractorYieldBackground * bg_up = new extractorYieldBackground(*ch,"Nominal",inputpath, outputpath, flags,"BG_UP");
+      extractorYieldBackground * bg_down = new extractorYieldBackground(*ch,"Nominal",inputpath, outputpath, flags,"BG_DOWN");
+      if(closure) {
+	bg_up->setClosureSample(closure_sample);
+	bg_down->setClosureSample(closure_sample);
       }
+
+      Double_t topmass_variation_bg_up = bg_up->getTopMass();
+      Double_t topmass_variation_bg_down = bg_down->getTopMass();
+
+      bg_up->getStatError(var_stat_pos,var_stat_neg);
+      bg_down->getStatError(var_stat_pos2,var_stat_neg2);
+
+      LOG(logINFO) << "BG_UP - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation_bg_up << " +" << var_stat_pos << " -" << var_stat_neg;
+      LOG(logINFO) << "BG_DOWN - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation_bg_down << " +" << var_stat_pos2 << " -" << var_stat_neg2;
+
+      Double_t delta_bg_up = (Double_t)topmass-topmass_variation_bg_up;
+      Double_t delta_bg_down = (Double_t)topmass-topmass_variation_bg_down;
+
+      LOG(logRESULT) << "BG_UP: delta = " << delta_bg_up << " GeV +" << systStatErr(total_stat_pos,var_stat_pos) << "-" << systStatErr(total_stat_neg,var_stat_neg);
+      LOG(logRESULT) << "BG_DOWN: delta = " << delta_bg_down << " GeV +" << systStatErr(total_stat_pos,var_stat_pos2) << "-" << systStatErr(total_stat_neg,var_stat_neg2);
+
+      getSystematicUpDownError(delta_bg_up,delta_bg_down,total_syst_pos,total_syst_neg);
+      
+      //if(syst->Contains("UP")) SystOutputFile << systab->writeSystematicsTableUp(*syst, delta, var_stat_pos, var_stat_neg);
+      //else SystOutputFile << systab->writeSystematicsTableDown(delta, var_stat_pos, var_stat_neg);
+
 
       // Systematic Variations produced by varying nominal samples:
       Double_t btag_syst_pos = 0, btag_syst_neg = 0;
+      Double_t jes_syst_pos = 0, jes_syst_neg = 0;
+
       Double_t btag_stat_pos = 0, btag_stat_neg = 0;
-      Double_t jes_flavor_pos = 0, jes_flavor_neg = 0;
-      Double_t jes_flavor_stat_pos = 0, jes_flavor_stat_neg = 0;
+      Double_t stat_up_pos = 0,   stat_up_neg = 0;
+      Double_t stat_down_pos = 0, stat_down_neg = 0;
 
-      Double_t total_jes_pos = 0, total_jes_neg = 0;
       for(std::vector<TString>::iterator syst = systematics.begin(); syst != systematics.end(); ++syst) {
-	LOG(logDEBUG) << "Getting " << (*syst) << " variation...";
-	extractorYield * variation_samples = new extractorYield(*ch,*syst,inputpath, outputpath, flags);
-	if(closure) variation_samples->setClosureSample(closure_sample);
+	LOG(logDEBUG) << "Getting " << (*syst) << "_UP/DOWN variation...";
 
-	Double_t topmass_variation = variation_samples->getTopMass();
-	LOG(logINFO) << *syst << " - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation;
-	Double_t delta = (Double_t)topmass-topmass_variation;
-	variation_samples->getStatError(var_stat_pos,var_stat_neg);
-	LOG(logRESULT) << *syst << ": delta = " << delta << " GeV +" << var_stat_pos << "-" << var_stat_neg;
-
-	if(syst->Contains("FLAVOR")) {
-	  // Adding up linearly, preserving the sign:
-	  if(delta > 0) jes_flavor_pos += delta;
-	  else jes_flavor_neg += delta;
-	  jes_flavor_stat_pos += var_stat_pos;
-	  jes_flavor_stat_neg += var_stat_neg;
-	}
-	else {
-	  // All others are summed quadratically directly to the total uncertainty:
-	  if(delta > 0) total_syst_pos += delta*delta;
-	  else total_syst_neg += delta*delta;
+	// Prepare the extraction modules for up and down variation:
+	extractorYield * variation_up = new extractorYield(*ch,(*syst)+"_UP",inputpath, outputpath, flags);
+	extractorYield * variation_down = new extractorYield(*ch,(*syst)+"_DOWN",inputpath, outputpath, flags);
+	if(closure) {
+	  variation_up->setClosureSample(closure_sample);
+	  variation_down->setClosureSample(closure_sample);
 	}
 
-	if(syst->Contains("BTAG")) {
-	  if(delta > 0) btag_syst_pos += delta*delta;
-	  else btag_syst_neg += delta*delta;
-	  btag_stat_pos += var_stat_pos;
-	  btag_stat_neg += var_stat_neg;
-	}
-	else if(syst->Contains("FLAVOR")) {}
-	else {
-	  if(syst->Contains("JES") && *syst != "JES_UP" && *syst != "JES_DOWN") {
-	    LOG(logCRITICAL) << "Adding " << *syst;
-	    if(delta > 0) total_jes_pos += delta*delta;
-	    else total_jes_neg += delta*delta;
+	// Extract the top quark mass from data:
+	Double_t topmass_variation_up = variation_up->getTopMass();
+	Double_t topmass_variation_down = variation_down->getTopMass();
+	variation_up->getStatError(stat_up_pos,stat_up_neg);
+	variation_down->getStatError(stat_down_pos,stat_down_neg);
+
+	LOG(logINFO) << *syst << "_UP   - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation_up << " +" << stat_up_pos << " -" << stat_up_neg;
+	LOG(logINFO) << *syst << "_DOWN - " << *ch << ": minimum Chi2 @ m_t=" << topmass_variation_down << " +" << stat_down_pos << " -" << stat_down_neg;
+
+	// Calculate difference to nominal extraction:
+	Double_t delta_up = (Double_t)topmass-topmass_variation_up;
+	Double_t delta_down = (Double_t)topmass-topmass_variation_down;
+
+	LOG(logRESULT) << *syst << "_UP:   delta = " << delta_up << " GeV +" << systStatErr(total_stat_pos,stat_up_pos) << "-" << systStatErr(total_stat_neg,stat_up_neg);
+	LOG(logRESULT) << *syst << "_DOWN: delta = " << delta_down << " GeV +" << systStatErr(total_stat_pos,stat_down_pos) << "-" << systStatErr(total_stat_neg,stat_down_neg);
+
+	bool systUpDown;
+	if(syst->Contains("BTAG")) { getSystematicUpDownError(delta_up,delta_down,btag_syst_pos,btag_syst_neg); }
+	else if(syst->Contains("JES_FLAVOR")) { getSystematicUpDownError(delta_up,delta_down,jes_syst_pos,jes_syst_neg); }
+	else { systUpDown = getSystematicUpDownError(delta_up,delta_down,total_syst_pos,total_syst_neg); }
+
+	if(!syst->Contains("BTAG") && !syst->Contains("JES_FLAVOR")) {
+	  if(systUpDown) { 
+	    if(abs(delta_up) > abs(delta_down)) { delta_down = 0; }
+	    else { delta_up = delta_down; delta_down = 0; }
 	  }
-	  if(syst->Contains("UP")) SystOutputFile << systab->writeSystematicsTableUp(*syst, delta, var_stat_pos, var_stat_neg);
-	  else SystOutputFile << systab->writeSystematicsTableDown(delta, var_stat_pos, var_stat_neg);
+
+	  SystOutputFile << systab->writeSystematicsTableUp(*syst, delta_up, 
+							    systStatErr(total_stat_pos,var_stat_pos),
+							    systStatErr(total_stat_neg,var_stat_neg));
+	  SystOutputFile << systab->writeSystematicsTableDown(delta_down,
+							      systStatErr(total_stat_pos,var_stat_pos),
+							      systStatErr(total_stat_neg,var_stat_neg));
 	}
       }
-      SystOutputFile << systab->writeSystematicsTableUp("JES_FLAVOR", TMath::Sqrt(jes_flavor_pos), jes_flavor_stat_pos/4, jes_flavor_stat_neg/4)
-		     << systab->writeSystematicsTableDown(TMath::Sqrt(jes_flavor_neg), jes_flavor_stat_pos/4, jes_flavor_stat_neg/4);
-
-      // Add JES_FLAVOR to the total uncertainty:
-      total_syst_pos += jes_flavor_pos*jes_flavor_pos;
-      total_syst_neg += jes_flavor_neg*jes_flavor_neg;
-      total_jes_pos += jes_flavor_pos*jes_flavor_pos;
-      total_jes_neg += jes_flavor_neg*jes_flavor_neg;
-
-      // Also writing the summed JES uncertainty for comparison, not added again (is already...)
-      SystOutputFile << systab->writeSystematicsTableUp("JES", TMath::Sqrt(total_jes_pos), 0, 0)
-		     << systab->writeSystematicsTableDown(TMath::Sqrt(total_jes_neg), 0, 0);
-
+      SystOutputFile << systab->writeSystematicsTableUp("JES_FLAVOR", TMath::Sqrt(jes_syst_pos), btag_stat_pos/12, btag_stat_neg/12)
+		     << systab->writeSystematicsTableDown(TMath::Sqrt(jes_syst_neg), btag_stat_pos/12, btag_stat_neg/12);
       SystOutputFile << systab->writeSystematicsTableUp("BTAG", TMath::Sqrt(btag_syst_pos), btag_stat_pos/12, btag_stat_neg/12)
 		     << systab->writeSystematicsTableDown(TMath::Sqrt(btag_syst_neg), btag_stat_pos/12, btag_stat_neg/12);
+      total_syst_pos += jes_syst_pos + btag_syst_pos;
+      total_syst_neg += jes_syst_neg + btag_syst_neg;
     }
 
     total_syst_pos = TMath::Sqrt(total_syst_pos);
