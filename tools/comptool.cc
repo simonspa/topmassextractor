23d3ec3f48bd46b57fff69bc2d61b9d4b17fb815
Simon Spannagel <simon.spannagel@desy.de>
2015-11-03 15:36:32 +0100

    Update plotting with comptool

--- tools/comptool.cc
+++ tools/comptool.cc
@@ -5,6 +5,7 @@
 #include "TH1.h"
 #include "TF1.h"
 #include "TStyle.h"
+#include "TGaxis.h"
 #include "TGraphAsymmErrors.h"
 #include <TROOT.h>
 
@@ -23,20 +24,22 @@ using namespace unilog;
 TH1D * getNiceHistogram(std::vector<Double_t> binning, TFile * file, TString hist, int nbins) {
 
   TH1D *sample = (TH1D*)(file->Get(hist)->Clone());
+  sample->Sumw2();
   TH1D *binned;
   if(!binning.empty()) {
     nbins = binning.size() - 1;
     LOG(logDEBUG2) << nbins << " bins. Rebinning now.";
-    binned = dynamic_cast<TH1D*>(sample->Rebin(nbins,"madgraph",&binning.front()));
+    binned = dynamic_cast<TH1D*>(sample->Rebin(nbins,"input",&binning.front()));
     for (Int_t bin=0; bin < nbins; bin++) {
       // Divide rebinned histogram's bin content by bin width factor (new/old):
       binned->SetBinError(bin+1,sqrt(binned->GetBinContent(bin+1))/((binning.at(bin+1)-binning.at(bin))/sample->GetBinWidth(1)));
       binned->SetBinContent(bin+1,binned->GetBinContent(bin+1)/((binning.at(bin+1)-binning.at(bin))/sample->GetBinWidth(1)));
+      LOG(logDEBUG3) << "Bin " << bin+1 << ": " << binned->GetBinContent(bin+1) << "+-" << binned->GetBinError(bin+1);
     }
   }
   else {
     LOG(logDEBUG2) << "Dividing binning by " << nbins << ". Rebinning now.";
-    binned = dynamic_cast<TH1D*>(sample->Rebin(nbins,"madgraph"));
+    binned = dynamic_cast<TH1D*>(sample->Rebin(nbins,"input"));
   }
 
   LOG(logDEBUG2) << "Rebinned, returning histogram ptr " << binned;
@@ -47,13 +50,13 @@ TH1D * getNiceHistogram(std::vector<Double_t> binning, TFile * file, TString his
 
 int main(int argc, char* argv[]) {
 
-
+  TGaxis::SetMaxDigits(3);
   Log::ReportingLevel() = Log::FromString("INFO");
 
   std::vector<std::string> files;
   std::vector<std::string> histograms;
   std::string outputpath = "default.root";
-  int rebinning = 50;
+  Int_t output_bins = 50;
   std::string datahistogram = "";
   bool have_data = false;
 
@@ -67,7 +70,7 @@ int main(int argc, char* argv[]) {
     // Set "data" flag to rebin from data histogram:
     else if(!strcmp(argv[i],"-d")) { have_data = true; datahistogram = string(argv[++i]); }
    // Set some number of bins to rebin to:
-    else if(!strcmp(argv[i],"-b")) { rebinning = atoi(argv[++i]); }
+    else if(!strcmp(argv[i],"-b")) { output_bins = atoi(argv[++i]); }
      // Set the verbosity level:
     else if (!strcmp(argv[i],"-v")) { Log::ReportingLevel() = Log::FromString(argv[++i]); }
     else { LOG(logERROR) << "Unrecognized command line argument \"" << argv[i] << "\".";}
@@ -77,20 +80,31 @@ int main(int argc, char* argv[]) {
   massextractor::setHHStyle(*gStyle);
   gStyle->SetOptStat(0);
   gStyle->SetOptTitle(0);
+  gROOT->ForceStyle();
+
+  // Open the outout file:
+  TFile output(outputpath.c_str(),"recreate");
+  gDirectory->pwd();
 
   // Loop over all requested histograms:
   for(std::vector<std::string>::iterator hgrm = histograms.begin(); hgrm != histograms.end(); hgrm++) {
 
     TString hist = *hgrm;
-
     LOG(logINFO) << "Plotting " << (*hgrm) << "...";
 
+    // Create a subdirectory in the output file
+    output.cd();
+    TDirectory *folder = output.mkdir(hist);
 
     // Get the reference sample:
     LOG(logDEBUG) << "Getting reference from " << files.front();
     TFile * referenceFile = new TFile(files.front().c_str());
     TH1D * reference;
-    TString referencename = getSampleLabel(files.front().c_str());
+    Int_t rebinning;
+    TString referencename;
+    if(have_data) referencename = "CMS Data";
+    else referencename = "MadGraph";
+    //else referencename = getSampleLabel(files.front().c_str());
 
     std::vector<Double_t> binning;
     // If we have data, get the binning from there:
@@ -111,18 +125,13 @@ int main(int argc, char* argv[]) {
     // Otherwise rebin to CLI option of to default:
     else {
       Int_t bins_now = ((TH1D*)referenceFile->Get(hist))->GetNbinsX();
-      rebinning = bins_now/rebinning;
+      rebinning = bins_now/output_bins;
       LOG(logDEBUG2) << "Reference has " << bins_now << " bins, dividing by " << rebinning;
-
       LOG(logDEBUG2) << "Fetching " << *hgrm;
       reference = getNiceHistogram(binning, referenceFile, hist, rebinning);
       LOG(logDEBUG2) << "Fetched successfully.";
-
-      // FIXME - no rebinning done!
     }
-    // Remove data file from others:
-    files.erase(files.begin());
-    LOG(logDEBUG2) << "Removed reference file from list, " << files.size() << " remaining.";
+    LOG(logDEBUG2) << "Skip reference file, " << files.size() << " files remaining.";
 
 
     LOG(logINFO) << "Fetching histograms " << *hgrm;
@@ -130,7 +139,7 @@ int main(int argc, char* argv[]) {
     std::vector<TH1D*> myhistograms;
     std::vector<TString> myhistogramnames;
     TH1D * tempBinned;
-    for(std::vector<std::string>::iterator file = files.begin(); file != files.end(); file++) {
+    for(std::vector<std::string>::iterator file = files.begin() + 1; file != files.end(); file++) {
       LOG(logDEBUG) << "Attempting to open " << *file;
       TFile * tempFile = new TFile((*file).c_str());
       LOG(logDEBUG2) << "Opened successfully, attempting to read " << *hgrm;
@@ -141,16 +150,18 @@ int main(int argc, char* argv[]) {
 
       // Get the name:
       TString name = (*file);
-      myhistogramnames.push_back(getSampleLabel(name));
+      if(name.Contains("powhegbox")) { myhistogramnames.push_back("POWHEG ttJ"); }
+      else if(name.Contains("powheg")) { myhistogramnames.push_back("POWHEG"); }
+      else { myhistogramnames.push_back(getSampleLabel(name)); }
       LOG(logDEBUG) << "Histogram read was: " << myhistogramnames.back();
 
       delete tempFile;
     }
 
-    TFile output(outputpath.c_str(),"recreate");
-    gDirectory->pwd();
+    // Descend into the folder:
+    folder->cd();
+    TCanvas *c = new TCanvas(hist,"");
     gStyle->SetOptStat(0);
-    TCanvas *c = new TCanvas(hist, "");
 
     TLegend *leg;
     if(hist.Contains("Rapidity")) { leg = new TLegend(0.15,0.6,0.4,.85); }
@@ -162,19 +173,29 @@ int main(int argc, char* argv[]) {
     massextractor::setStyle(reference);
     gStyle->SetOptStat(0);
     gStyle->SetOptTitle(0);
-    reference->SetLineColor(kGray+2);
+    if(!have_data) reference->SetMarkerStyle(0);
+    setStyle(reference,"reference");
+    reference->SetLineColor(kBlack);
     reference->SetLineWidth(2);
+    reference->SetTitle("");
     leg->AddEntry(reference,referencename,"lp");
 
     // Set axis label:
-    if(hist.Contains("Eta")) reference->GetXaxis()->SetTitle("#eta");
-    else if(hist.Contains("pT")) reference->GetXaxis()->SetTitle("p_{T} [GeV]");
-    else if(hist.Contains("1stJetMass")) reference->GetXaxis()->SetTitle("#rho_{s}");
-    else if(hist.Contains("Mass")) reference->GetXaxis()->SetTitle("m_{tt#bar} [GeV]");
-    else if(hist.Contains("Rapidity")) reference->GetXaxis()->SetTitle("Rapidity");
+    std::string quantity = "";
+    std::string unit = "";
+    if(hist.Contains("Eta")) {             quantity = "#eta"; }
+    else if(hist.Contains("pT")) {         quantity = "p_{T}"; unit = " [GeV]"; }
+    else if(hist.Contains("1stJetMass")) { quantity = "#rho_{s}"; }
+    else if(hist.Contains("Mass")) {       quantity = "m_{tt#bar}"; unit = " [GeV]"; }
+    else if(hist.Contains("Rapidity")) {   quantity = "Rapidity"; }
+    reference->GetXaxis()->SetTitle(Form("%s%s",quantity.c_str(),unit.c_str()));
+    reference->GetYaxis()->SetTitle(Form("#frac{1}{#sigma} #frac{d#sigma}{d%s}",quantity.c_str()));
+    reference->GetYaxis()->SetTitleOffset(1.5);
+    reference->GetYaxis()->SetLabelOffset(0.01);
 
-    reference->Draw();
-    reference->Write();
+    reference->Draw("e");
+    DrawFreeCMSLabels("Simulation");
+    reference->Write(referencename);
 
     std::vector<TH1*> denominators;
 
@@ -184,12 +205,20 @@ int main(int argc, char* argv[]) {
       LOG(logDEBUG2) << "Attempting to plot histogram " << i;
       TH1D* plothist = myhistograms.at(i);
       LOG(logDEBUG2) << "Histogram pointer: " << plothist;
-      plothist->SetMarkerColor(i+2);
-      plothist->SetLineColor(i+2);
+      if(i == 0) {
+	plothist->SetMarkerStyle(0);
+	plothist->SetLineColor(kGray+2);
+	plothist->SetLineStyle(7);
+      }
+      else if(i == 1) {
+	plothist->SetLineColor(kRed+1);
+	plothist->SetMarkerStyle(0);
+      }
+      else plothist->SetLineColor(i+2);
       plothist->SetLineWidth(2);
       leg->AddEntry(plothist,myhistogramnames.at(i),"l");
-      plothist->Draw("samel");
-      plothist->Write();
+      plothist->Draw("same e");
+      plothist->Write(myhistogramnames.at(i));
       if(i < 7) denominators.push_back(plothist);
     }
     leg->Draw();
